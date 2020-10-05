6c3761e9c239fe1347e9291c1bec31d88c90103e
jaiswal-purvesh <purveshmjaiswal@gmail.com>
2020-10-05 13:50:12 +0530

    remove sudo

--- json-report-auto.sh
+++ json-report-auto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo ps -ef | grep logstash | grep -v grep | awk '{print $2}'
-sudo ps -ef | grep logstash | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep logstash | grep -v grep | awk '{print $2}' | xargs kill
 cd /usr/share/logstash/bin/
 ./logstash -f /etc/logstash/conf.d/json-report.conf &
