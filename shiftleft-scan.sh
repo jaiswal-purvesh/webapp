d1437ed937404c7e3734e5d0cc8796739e9db762
jaiswal-purvesh <purveshmjaiswal@gmail.com>
2020-10-08 01:04:36 +0530

    remove docker pull

--- shiftleft-scan.sh
+++ shiftleft-scan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Make sure we are using the latest version
-docker pull shiftleft/sast-scan
+#docker pull shiftleft/sast-scan
 
 docker run --rm -e "WORKSPACE=${PWD}" -v $PWD:/app shiftleft/sast-scan scan
