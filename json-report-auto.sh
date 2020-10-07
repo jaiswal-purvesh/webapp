36c6b65771251d6fb0ce1118a013517988d2770d
jaiswal-purvesh <purveshmjaiswal@gmail.com>
2020-10-08 01:30:59 +0530

    add logstash service

--- json-report-auto.sh
+++ json-report-auto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo ps -ef | grep logstash | grep -v grep | awk '{print $2}' | xargs kill
-logstash -f /etc/logstash/conf.d/json-scan.conf &
+sudo service logstash stop
+sudo service logstash start
